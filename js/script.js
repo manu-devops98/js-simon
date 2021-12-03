f9dc0ed4e93ab460043ca0082d43cd5a9544b5a5
manu-devops98 <manuelcappello7@gmail.com>
2021-12-03 17:01:14 +0100

    add esercizio con errore nel conteggio secondi

--- js/script.js
+++ js/script.js
@@ -1,13 +1,25 @@
 /* Descrizione:
-Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
+Visualizzare in pagina 5 numeri casuali poi fateli sparire.
+ Da lì parte un timer di 30 secondi.
 Dopo 30 secondi l’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
 Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati. */
 
 // prendo il container html 
 const container = document.querySelector('.container');
 const counter = document.querySelector('.counter');
-    // inserisco nel container 5 numeri random:
-    //   - creo ciclo per generare 5 numeri random in array:
+//     // inserisco nel container 5 numeri random:
+//     //   - creo ciclo per generare 5 numeri random in array:
+    let seconds = 30;
+const timer = setInterval(() => {
+    if (seconds > 0) {
+        seconds -= 1;
+        container.innerHTML = `<h1>Tempo rimasto: ${seconds}</h1>`;
+    } else if (seconds == 0) {
+        clearInterval(timer); 
+        container.innerHTML = ''; 
+    }
+}, 2000);
+
 let numbRand = [];
 for (let i = 0; i < 5; i++) {
     numbRand.push(getRndInteger(1,100));
@@ -24,50 +36,20 @@ for (let i = 0; i < 5; i++) {
         console.log('usernumber',userNumber);//consolelog
             if (numbRand.includes(userNumber)) {
                 numbersCorrect += 1;
-                if (numbersCorrect == 5) {
+                if (numbersCorrect == numbRand.length) {
                     container.innerHTML = `<h1>Congratulazioni, hai indovinato tutti i numeri</h1>`;
                 console.log('numbersCorrect if',numbersCorrect);//consolelog 
-                } else if (numbersCorrect != numbRand.length) {
+                } else if (numbersCorrect < numbRand.length) {
                     container.innerHTML = `<h1>Peccato, hai indovinato ${numbersCorrect} numeri : ${arrayUserNumber}</h1>`;
                 console.log('numbers else', numbersCorrect);//consolelog            
                 } 
-            } else {
-                container.innerHTML = `<h1>Peccato, non hai indovinato nessun numero</h1>`;
-            }
+            } else if (!(numbRand.includes(userNumber))) {
+                container.innerHTML = `<h1>Peccato, hai indovinato ${numbersCorrect} numeri : ${arrayUserNumber}</h1>`;
+            } 
         }
-    }, 32 * 1000); 
+    }, 33 * 1000); 
     } 
 }
-    // creo timer di 30 secondi:
-        // - timer parte d 30 secondi poi decresce
-        // -- timer arriva a zero allora blocco il timer
-let seconds = 30;
-const timer = setInterval(() => {
-    if (seconds > 0) {
-        seconds -= 1;
-        counter.innerHTML = `<h1>Tempo rimasto: ${seconds}</h1>`;
-        container.append(counter);
-
-    } else if (seconds == 0) {
-        clearInterval(timer); 
-        container.innerHTML = ''; 
-    }
-}, 1000);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
     // FUNZIONI         
 function getRndInteger(min, max) {
