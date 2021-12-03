9b3336b413c093444fc0cbe7f58ea347339d6a2f
manu-devops98 <manuelcappello7@gmail.com>
2021-12-03 15:59:02 +0100

    add esercizio completo

--- js/script.js
+++ js/script.js
@@ -11,10 +11,31 @@ const counter = document.querySelector('.counter');
 let numbRand = [];
 for (let i = 0; i < 5; i++) {
     numbRand.push(getRndInteger(1,100));
-    console.log(numbRand);
-    container.innerHTML += `<h2 class="number">${numbRand[i]}</h2>`;  
+    console.log(numbRand); //consolelog
+    container.innerHTML += `<h2 class="number">${numbRand[i]}</h2>`;
+    if (numbRand.length == 5) {
+       setTimeout(() => {
+           let numbersCorrect = 0;
+           let arrayUserNumber = [];
+        for (let x = 1; x <= 5; x++) {
+            const userNumber = parseInt(prompt(`inserisci numero visualizzato`));
+            arrayUserNumber.push(userNumber);
+        console.log(arrayUserNumber);//consolelog
+        console.log('usernumber',userNumber);//consolelog
+            if (numbRand.includes(userNumber)) {
+                numbersCorrect += 1;
+                if (numbersCorrect == 5) {
+                    container.innerHTML = `<h1>Congratulazioni, hai indovinato tutti i numeri</h1>`;
+                console.log('numbersCorrect if',numbersCorrect);//consolelog 
+                } else if (numbersCorrect != 5) {
+                    container.innerHTML = `<h1>Peccato, hai indovinato ${numbersCorrect} numeri : ${arrayUserNumber}</h1>`;
+                console.log('numbers else', numbersCorrect);//consolelog            
+                } 
+            }
+        }
+    }, 32 * 1000); 
+    } 
 }
-
     // creo timer di 30 secondi:
         // - timer parte d 30 secondi poi decresce
         // -- timer arriva a zero allora blocco il timer
@@ -30,6 +51,7 @@ const timer = setInterval(() => {
         container.innerHTML = ''; 
     }
 }, 1000);
+
 
 
 
